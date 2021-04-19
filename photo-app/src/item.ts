78a8896dbc311ccdec95837b598500e0c28aa3cb
Agustina Corvo Gutierrez <agustina.corvo@fing.edu.uy>
2021-04-19 15:12:53 -0300

    fix

--- photo-app/src/item.ts
+++ photo-app/src/item.ts
@@ -1,6 +1,7 @@
 export abstract class Item {
-  protected readonly id: number;
+  public readonly id: number;
   protected title: string;
+
   constructor(id: number, title: string) {
     this.id = id;
     this.title = title;
